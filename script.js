e6899f0450257f28466a325ff55a8e6cc698dd4f
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-08-19 19:01:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your code here
+const divs = document.querySelectorAll('.image');
+
+let draggedDiv = null;
+
+divs.forEach(div => {
+  div.addEventListener('dragstart', dragStart);
+  div.addEventListener('dragover', dragOver);
+  div.addEventListener('drop', drop);
+});
+
+function dragStart(e) {
+  draggedDiv = e.target;
+  e.dataTransfer.setData('text/html', draggedDiv.outerHTML);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  if (!draggedDiv) return;
+
+  e.preventDefault();
+
+  const dropTarget = e.target.closest('.image');
+
+  if (dropTarget && dropTarget !== draggedDiv) {
+    const tempHTML = dropTarget.outerHTML;
+    dropTarget.outerHTML = draggedDiv.outerHTML;
+    draggedDiv.outerHTML = tempHTML;
+
+    divs.forEach(div => {
+      div.addEventListener('dragstart', dragStart);
+      div.addEventListener('dragover', dragOver);
+      div.addEventListener('drop', drop);
+    });
+  }
+}
